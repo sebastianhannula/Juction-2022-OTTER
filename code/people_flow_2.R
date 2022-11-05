d7696a0057fd335ec5527f689ebf2dec42cc8be6
lotttakm <117508437+lotttakm@users.noreply.github.com>
2022-11-05 21:54:16 +0200

    Update people_flow_2.R

--- code/people_flow_2.R
+++ code/people_flow_2.R
@@ -28,5 +28,26 @@ ennuste = ts(ennuste, start = c(35,1), frequency = 24)
 clyla = ts(clyla, start = c(35,1), frequency = 24)
 clala = ts(clala, start = c(35,1), frequency = 24)
 ts.plot(ennuste, col = c("blue"))
+ennuste[ennuste <0] <- 0
 
-write.csv(ennuste, "people_flow_pred_nov.csv")
+write.csv(ennuste, file="people_flow_pred_nov.csv")
+
+#Create binary vector and constant factor normalized to maximum employee count
+
+df_binary <- df
+df_binary[df_binary > 0] <- 1
+write.csv(df_binary, "people_flow_binary.csv")
+
+ennuste_binary <- ennuste
+as.data.frame(ennuste_binary)
+ennuste_binary[ennuste_binary > 0] <- 1
+write.csv(ennuste_binary, "people_flow_pred_binary.csv")
+
+df_constant <- df
+empl <- max(df_constant)
+df_final <- df_constant / empl
+write.csv(df_final, "people_constant_factor.csv")
+
+df_pred_constant <- ennuste
+df_pred_final <- df_pred_constant / empl
+write.csv(df_pred_final, "people_pred_constant_factor.csv")
